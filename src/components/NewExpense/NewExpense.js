b2fdded321277422d5a17b275e3ef22d16ebb68a
PerizatSabitbek <psabitbek@magnumtech.kz>
2021-10-21 15:45:50 +0600

    add new expense button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react/cjs/react.development";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [form, setForm] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setForm(false)
   };
 
+  const startEditingHandler = () => {
+    setForm(true);
+  };
+  const stopEditingHandler = () => {
+    setForm(false);
+  };
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    <div className="new-expense">
+      {!form ? (
+        <div className="new-expense__actions-add">
+          <button type="submit" onClick={startEditingHandler}>
+            Add New Expense
+          </button>
+        </div>
+      ) : (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
